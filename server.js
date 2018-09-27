57f9f13fca70df47ec03447d0c07f9ef6ce9f2d0
j-rods <jrrodriguez84@gmail.com>
2018-09-27 12:16:54 +0100

    add basic typeDefs to server js

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
 
-const server = new ApolloServer({});
+const todos = [
+    { task: 'Setup', completed: false },
+    { task: 'Run', completed: true }
+];
+
+const typeDefs = gql`
+    type Todo {
+        task: String
+        completed: Boolean
+    }
+`;
+
+const server = new ApolloServer({
+    typeDefs: typeDefs
+});
 
 server.listen();
