4e4f4ed6b97d97231fe2bf17244ccef77e5cdc17
j-rods <jrrodriguez84@gmail.com>
2018-10-19 14:12:43 +0100

    comment tasks

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
 
+// import typeDefs
 const filePath = path.join(__dirname, 'typeDefs.gql');
 const typeDefs = fs.readFileSync(filePath, 'utf-8');
 const resolvers = require('./resolvers');
 
+// import env variables
 require('dotenv').config({path: 'variables.env'});
 const User = require('./models/User');
 const Post = require('./models/Post');
 
+
+// connect to mlab db
 mongoose.connect(
     process.env.MONGO_URI,
     { useNewUrlParser: true }
@@ -18,6 +22,7 @@ mongoose.connect(
 .then(() => console.log('DB connected'))
 .catch(err => console.error(err));
 
+// create apollo/graphql server using typedefs/resolvers/context object
 const server = new ApolloServer({
     typeDefs,
     resolvers,
